20f52b8aa93d56df3667eaa5ad02563c1ac7a436
sambodhi-zade <82253573+sambodhi-zade@users.noreply.github.com>
2021-06-07 12:12:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-32
+# PROJECT-29
---
+++ ground.js
@@ -0,0 +1,17 @@
+class Ground {
+    constructor() {
+      var options = {
+          isStatic: true
+      }
+     
+      this.ground = Bodies.rectangle(750,390,900,20,options);
+      World.add(world, this.ground);
+    }
+    display(){
+      strokeWeight(2);
+      fill("brown");
+      rectMode(CENTER);
+      
+      rect(this.ground.position.x, this.ground.position.y,900,20);
+    }
+  };
